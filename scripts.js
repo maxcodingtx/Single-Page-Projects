b083f00e33df62a91ba33fd2a9e63e6982c8b990
Max <maxcodingtx@gmail.com>
2024-05-07 12:36:55 -0500

    added 'active' function

--- scripts.js
+++ scripts.js
@@ -8,3 +8,20 @@ function totalClicks() {
     count += 1;
     totalClicks.innerHTML = count
 };
+
+
+function active(event) {
+    var active = document.getElementsByClassName('is-active')
+
+    if (active.length > 0) {
+        active[0].className = active[0].className.replace(' is-active', '');
+    }
+    event.target.className += ' is-active'
+
+}
+
+
+
+
+
+
