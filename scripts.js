79e6580fafbcfd7e56df97722a7bd9e4e048cdc8
Max <maxcodingtx@gmail.com>
2024-05-07 12:50:40 -0500

    added comments to scripts

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 
-
 var count = 0;
 
+// calculates total amount of times button has been clicked on
 function totalClicks() {
     var totalClicks = document.getElementById('totalClicks')
 
@@ -9,19 +9,16 @@ function totalClicks() {
     totalClicks.innerHTML = count
 };
 
-
+// adds 'is-active' class when clicked on
 function active(event) {
     var active = document.getElementsByClassName('is-active')
 
+    // if another element has 'is-active' class, remove it
     if (active.length > 0) {
         active[0].className = active[0].className.replace(' is-active', '');
     }
+    // then, add 'is-active' class to element
     event.target.className += ' is-active'
 
 }
-
-
-
-
-
 
