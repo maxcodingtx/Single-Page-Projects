fe0df73a0486ffa46edb4eef3ecf6df38ff2db18
maxcodingtx <maxcodingtx@gmail.com>
2024-09-05 18:27:47 -0500

    made 'home' tab default by with an arrow function on window load

--- scripts.js
+++ scripts.js
@@ -1,18 +1,25 @@
 
-function openTab(evt, cityName) {
-    var i, tabContent, tabLinks;
+// 'home' tab selected by default
+window.onload = () => {
+    document.getElementById('homeLink').click()
+}
 
-    tabContent = document.getElementsByClassName("tabContent");
+// function which displays tab when clicked on
+function openTab(evt, cityName) {
+
+    // setting tabContent divs to not be displayed (display: none)
+    var tabContent = document.getElementsByClassName("tabContent");
     for (i=0; i<tabContent.length; i++) {
         tabContent[i].style.display = "none";
     }
 
-    tabLinks = document.getElementsByClassName("tabLink");
+    // removing 'active class' from tab links (nav buttons)
+    var tabLinks = document.getElementsByClassName("tabLink");
     for (i=0; i<tabLinks.length; i++) {
         tabLinks[i].className = tabLinks[i].className.replace(" active", "")
     }
 
-    
+    //show current tab and add 'active' class to button that currently selected
     document.getElementById(cityName).style.display = "block"
     evt.currentTarget.className += " active"
 }
