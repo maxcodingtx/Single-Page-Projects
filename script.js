dfbcff4d36420e2ee5120d526c0e0232ba3780c6
maxcodingtx <maxcodingtx@gmail.com>
2024-12-09 12:26:36 -0600

    added comments

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 
 const BUTTONS_RIGHT = document.querySelectorAll('.scroll-button-right');
+
 BUTTONS_RIGHT.forEach(button => {
     button.addEventListener('click', (evt) => {
         var buttonId = evt.target.id
+        // getting the number in the class name of button and implicitly coercing it.
         var buttonNumber = buttonId.slice(-1)*1
+        // using the number above to link the button and the div it will scroll on.
         var container = document.getElementById(`card-container${buttonNumber}`)
         container.scrollLeft += 250
     });
@@ -13,7 +16,9 @@ const BUTTONS_LEFT = document.querySelectorAll('.scroll-button-left');
 BUTTONS_LEFT.forEach(button => {
     button.addEventListener('click', (evt) => {
         var buttonId = evt.target.id
+        // getting the number in the class name of button and implicitly coercing it.
         var buttonNumber = buttonId.slice(-1)*1
+        // using the number above to link the button and the div it will scroll on.
         var container = document.getElementById(`card-container${buttonNumber}`)
         container.scrollLeft -= 250
     });
