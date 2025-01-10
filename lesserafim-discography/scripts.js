6e555c5a07efd3a97479a2ded3b75910a66e374e
maxcodingtx <maxcodingtx@gmail.com>
2025-01-09 20:21:41 -0600

    added interactivity to show/hide button for tracklists.

--- lesserafim-discography/scripts.js
+++ lesserafim-discography/scripts.js
@@ -1,2 +1,26 @@
 
 
+class Music {
+    constructor(buttonSelector, trackListSelector) {
+        this.button = document.querySelector(buttonSelector);
+        this.trackList = document.getElementById(trackListSelector);
+        this.init()
+    }
+
+    init() {
+        this.button.addEventListener('click', () => {
+            this.toggle();
+    });}
+
+    toggle() {
+        if (this.trackList.classList.contains('is-visible')) {
+            this.trackList.classList.remove('is-visible');
+            this.button.innerHTML = 'Show';
+        } else {
+            this.trackList.classList.add('is-visible');
+            this.button.innerHTML = 'Hide';
+        }
+    }
+}
+
+const music = new Music('.button',`trackList${this.button.dataset.track}`);
