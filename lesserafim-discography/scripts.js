92863eefd3a7b6d7fa19b3a56bec3c3c971af68f
maxcodingtx <maxcodingtx@gmail.com>
2025-01-11 00:05:39 -0600

    changed js functionality to better cover all future and current tracklists

--- lesserafim-discography/scripts.js
+++ lesserafim-discography/scripts.js
@@ -1,26 +1,55 @@
 
 
+// class Music {
+//     constructor(buttonSelector, trackListSelector) {
+//         this.button = document.querySelector(buttonSelector);
+//         this.trackList = document.getElementById(trackListSelector);
+//         this.currentButton = `${buttonSelector}${this.button.dataset.track}`;
+//         this.init()
+//     }
+
+//     init() {
+//         this.button.addEventListener('click', () => {
+//             this.toggle();
+//     });}
+
+//     toggle() {
+//         if (this.trackList.classList.contains('is-visible')) {
+//             this.trackList.classList.remove('is-visible');
+//             this.button.innerHTML = 'Show';
+//         } else {
+//             this.trackList.classList.add('is-visible');
+//             this.button.innerHTML = 'Hide';
+//         }
+//     }
+// }
+
+
 class Music {
-    constructor(buttonSelector, trackListSelector) {
-        this.button = document.querySelector(buttonSelector);
-        this.trackList = document.getElementById(trackListSelector);
-        this.init()
+    constructor(buttonSelector) {
+        this.buttons = document.querySelectorAll(buttonSelector);
+        this.addEventListeners();
     }
 
-    init() {
-        this.button.addEventListener('click', () => {
-            this.toggle();
-    });}
+    addEventListeners() {
+        this.buttons.forEach(button => {
+            button.addEventListener('click', (event) => this.toggle(event));
+        });
+    }
 
-    toggle() {
-        if (this.trackList.classList.contains('is-visible')) {
-            this.trackList.classList.remove('is-visible');
-            this.button.innerHTML = 'Show';
+    toggle(event) {
+        const button = event.target;
+        const trackList = button.dataset.track;
+        const trackListElement = document.getElementById(trackList);
+
+        if (trackListElement.classList.contains('is-visible')) {
+            trackListElement.classList.remove('is-visible');
+            button.innerHTML = 'Show';
         } else {
-            this.trackList.classList.add('is-visible');
-            this.button.innerHTML = 'Hide';
+            trackListElement.classList.add('is-visible');
+            button.innerHTML = 'Hide';
         }
     }
 }
 
-const music = new Music('.button',`trackList${this.button.dataset.track}`);
+const music = new Music('.button');
