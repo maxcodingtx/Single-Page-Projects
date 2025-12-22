715dd7b177e5ae7376659e1ac52479c480006b13
maxcodingtx <maxcodingtx@gmail.com>
2025-12-22 01:30:48 -0600

    create script to replace time option li elements with dynamic template li elements.
    Instead of manual HTML li "option" elements, I have replaced them with an already created <template> tag replacement.

---
+++ cps-website/scripts/timeOptionTemplate.js
@@ -0,0 +1,29 @@
+
+const template = document.getElementById("timeOptionTemplate")
+const timeOptionsContainer = document.getElementById("timeOptionsContainer")
+
+// data for buttons (1s, 5s, 15s, 30s, 45s, 60s)
+const timeOptionData = [
+  { title: "1 Second", id: "1s" },
+  { title: "5 Seconds", id: "5s" },
+  { title: "15 Seconds", id: "15s" },
+  { title: "30 Seconds", id: "30s" },
+  { title: "45 Seconds", id: "45s" },
+  { title: "60 Seconds", id: "60s" }
+]
+
+function createTimeOptions(data) {
+  const clone = template.content.cloneNode(true)
+
+  const timeOptionTitle = clone.querySelector(".time__option")
+
+
+  timeOptionTitle.textContent = data.title 
+  timeOptionTitle.id = data.id
+  timeOptionsContainer.appendChild(clone)
+  timeOptionTitle.addEventListener("click", activeClassHandling)
+}
+
+timeOptionData.forEach(createTimeOptions)
+
+// handle active class handling
