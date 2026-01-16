b60f75a8f99bf5d23b36efb0462978cd92209143
maxcodingtx <maxcodingtx@gmail.com>
2026-01-15 18:40:42 -0600

    refactor: move flex direction and aligning functions for flexbox utility classes into new file.

---
+++ flexbox-guide/scripts/flex-directions.js
@@ -0,0 +1,82 @@
+// FLEX DIRECTION FUNCTIONS
+// event listeners in HTML
+
+function directionRow() {
+  applyFlexClass("direction", "flex-direction-row");
+}
+
+function directionColumn() {
+  applyFlexClass("direction", "flex-direction-column");
+}
+
+function directionRowReverse() {
+  applyFlexClass("direction", "flex-direction-row-reverse");
+}
+
+function directionColumnReverse() {
+  applyFlexClass("direction", "flex-direction-column-reverse");
+}
+
+function alignitemsStart() {
+  applyFlexClass("alignItems", "align-items-flex-start");
+}
+
+function alignitemsCenter() {
+  applyFlexClass("alignItems", "align-items-center");
+}
+
+function alignitemsEnd() {
+  applyFlexClass("alignItems", "align-items-flex-end");
+}
+
+function alignitemsStretch() {
+  applyFlexClass("alignItems", "align-items-stretch");
+}
+
+function justifycontentStart() {
+  applyFlexClass("justifyContent", "justify-content-start");
+}
+
+function justifycontentCenter() {
+  applyFlexClass("justifyContent", "justify-content-center");
+}
+
+function justifycontentEnd() {
+  applyFlexClass("justifyContent", "justify-content-end");
+}
+
+function justifycontentBetween() {
+  applyFlexClass("justifyContent", "justify-content-between");
+}
+
+function justifycontentAround() {
+  applyFlexClass("justifyContent", "justify-content-around");
+}
+
+function justifycontentEvenly() {
+  applyFlexClass("justifyContent", "justify-content-evenly");
+}
+
+function aligncontentStart() {
+  applyFlexClass("alignContent", "align-content-start");
+}
+
+function aligncontentCenter() {
+  applyFlexClass("alignContent", "align-content-center");
+}
+
+function aligncontentEnd() {
+  applyFlexClass("alignContent", "align-content-end");
+}
+
+function aligncontentStretch() {
+  applyFlexClass("alignContent", "align-content-stretch");
+}
+
+function aligncontentBetween() {
+  applyFlexClass("alignContent", "align-content-between");
+}
+
+function aligncontentAround() {
+  applyFlexClass("alignContent", "align-content-around");
+}
