170def0b6133d8d550541eca4992c1e60d2faf81
maxcodingtx <maxcodingtx@gmail.com>
2026-01-18 02:24:45 -0600

    feat: add mermaid.js for diagram rendering within learn more section.

---
+++ flexbox-guide/scripts/mermaid.js
@@ -0,0 +1,18 @@
+import mermaid from "https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.min.mjs";
+
+mermaid.initialize({
+  startOnLoad: true,
+  theme: "dark",
+  themeVariables: {
+    primaryColor: "#5d00ff",
+    primaryTextColor: "#fff",
+    primaryBorderColor: "#7a2bff",
+    lineColor: "#7a2bff",
+    secondaryColor: "#252525",
+    tertiaryColor: "#171717",
+    background: "#171717",
+    mainBkg: "#252525",
+    secondBkg: "#171717",
+    darkMode: true,
+  },
+});
